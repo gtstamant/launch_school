93a27ec463e6e2df11b193ca8bb6b09f7f8fdc5a
Guy St Amant <gtstamant@gmail.com>
2024-03-03 09:53:23 -0500

    'Resolve merge issue'

--- lesson_2/mortgage_calculator/mortgage_calculator.py
+++ lesson_2/mortgage_calculator/mortgage_calculator.py
@@ -11,52 +11,15 @@ def prompt(message):
     print_val = messages(message)
     print(print_val)
 
-<<<<<<< HEAD
-def invalid_input(num):
-    try:
-        num = float(num)
-        if num <= 0:
-            raise ValueError(f"Value must be > 0: {num}")
-=======
 def check_input(num):
     try:
         num = float(num)
         if num <= 0:
             raise ValueError
->>>>>>> development
     except ValueError:
         return True
     return False
 
-<<<<<<< HEAD
-def collect_loan_data():
-    prompt("PRINCIPAL")  
-    principal = input()
-    while invalid_input(principal):
-        prompt("INVAL_NUM")
-        principal = input()
-    
-    prompt("APR")
-    annual_interest = input().replace('%', '')
-    while invalid_input(annual_interest):
-        prompt("INVAL_PERC")
-        annual_interest = input().replace('%', '')
-
-    prompt("DURATION")
-    loan_time = input()
-    while invalid_input(loan_time):
-        prompt("INVAL_NUM")
-        loan_time = input()
-    
-    principal = float(principal)
-    annual_interest = float(annual_interest) / 100
-    loan_time = float(loan_time)
-
-    return (principal, annual_interest, loan_time)
-
-def monthly_rate(annual_rate):
-    return annual_rate / 12
-=======
 def collect_datum(data_type):
     prompt(data_type)
     datum = input().replace('%', '')
@@ -77,7 +40,6 @@ def collect_loan_data(data_list):
 
 def monthly_rate(annual_rate):
     return (annual_rate / 100) / 12
->>>>>>> development
 
 def monthly_duration(annual_duration):
     return annual_duration * 12
@@ -90,13 +52,6 @@ def loan_payment(amount, interest, duration):
 def total_interest(amount, duration, payments):
     return (duration * payments) - amount
 
-<<<<<<< HEAD
-def display_data(duration, monthly_payment, interest_payment):
-    data = messages('DATA').format(
-        monthly_payment=monthly_payment, 
-        duration=duration, 
-        interest_payment=interest_payment
-=======
 def generate_monthly_info(loan_data_dict):
     monthly_data = {}
     monthly_data['amount'] = loan_data_dict['PRINCIPAL']
@@ -119,7 +74,6 @@ def display_data(monthly_data):
         monthly_payment=monthly_data['month_payment'], 
         duration=monthly_data['month_duration'], 
         interest_payment=monthly_data['tot_interest']
->>>>>>> development
     )
     print(data)
 
@@ -130,20 +84,6 @@ def get_user_choice():
         user_choice = input()
     return user_choice
 
-<<<<<<< HEAD
-def run_calculator():
-    prompt('WELCOME')
-    while True:
-        prompt("START")
-        
-        principal, apr, duration = collect_loan_data()
-        month_rate = monthly_rate(apr)
-        month_duration = monthly_duration(duration)
-        month_payment = loan_payment(principal, month_rate, month_duration)
-        interest = total_interest(principal, month_duration, month_payment)
-        
-        display_data(month_duration, month_payment, interest)
-=======
 def run_calculator(starting_conditions):
     prompt('WELCOME')
     while True:
@@ -151,7 +91,6 @@ def run_calculator(starting_conditions):
         loan_data = collect_loan_data(starting_conditions)
         monthly_data = generate_monthly_info(loan_data)        
         display_data(monthly_data)
->>>>>>> development
         prompt('DECISION')
         decision = get_user_choice()
         if decision[0].lower() == 'n':
@@ -159,10 +98,6 @@ def run_calculator(starting_conditions):
             break
         os.system('clear')
 
-<<<<<<< HEAD
-run_calculator()
-=======
 starting_conditions = ('PRINCIPAL', 'APR', 'DURATION')
 
 run_calculator(starting_conditions)
->>>>>>> development
