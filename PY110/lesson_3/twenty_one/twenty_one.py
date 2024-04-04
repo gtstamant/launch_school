30e6f751f21c1813d5b18e8bc608cd499b8022f2
Guy St Amant <gtstamant@gmail.com>
2024-04-04 12:18:10 -0400

    Improved function naming in twenty one

--- PY110/lesson_3/twenty_one/twenty_one.py
+++ PY110/lesson_3/twenty_one/twenty_one.py
@@ -6,14 +6,15 @@ import os
 SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
 FACE_VALUES = {'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
 RANKS = [num for num in range(2, 11)] + [key for key in FACE_VALUES.keys()]
-HAND_SIZE = 2
+INITIAL_HAND_SIZE = 2
 VALID_MOVES = ['hit', 'stay', 'h', 's']
+USER_DECISION = ['yes', 'no', 'y', 'n']
 HIGH_ACE_VALUE = 11
 LOW_ACE_VALUE = 1
 MAX_HAND = 21
 DEALER_MAX = 17
 
-def prompt(message):
+def prompt(message): # Better name? print_w_arrow
     print(f'==> {message}')
 
 def clear_screen():
@@ -30,7 +31,7 @@ def deal_hands(deck):
     dealer_hand = []
     
     shuffled_deck = random.sample(deck, k=len(deck))
-    for card in range(HAND_SIZE * 2):
+    for card in range(INITIAL_HAND_SIZE * 2):
         if card % 2 == 0:
             player_hand.append(shuffled_deck.pop())
         else:
@@ -41,13 +42,7 @@ def deal_hands(deck):
 def hit(deck, hand):
     hand.append(deck.pop())
 
-def get_dealer_hits(deck, hand):
-    while get_hand_value(hand) < DEALER_MAX:
-        hit(deck, hand)
-
-    return hand
-
-def split_hand(hand):
+def split_hand(hand): # get_aces_and_remainder; nested function w/below
     hand_ex_aces, aces = [], []
     for card_info in hand:
         for card in card_info.keys():
@@ -80,50 +75,52 @@ def get_hand_value(hand):
 def is_bust(hand):
     return get_hand_value(hand) > MAX_HAND
 
-def is_valid_move(): # Change name as not boolean
-    while True:
-        user_input = input().casefold() 
-        if user_input in VALID_MOVES:
-            return user_input
-        prompt("Sorry, that's not a valid input! Try again:")
+def is_valid_move(user_move): # Change name as not boolean
+    return user_move in VALID_MOVES     
 
-def get_player_move(): # Need to update this!!!
-    prompt('Would you like to hit or stay?')
-    return is_valid_move()[0]
+def prompt_player_move(): # Need to update this!!!
+    while True:
+        prompt('Would you like to hit or stay?')
+        user_input = input().casefold()
+        
+        if is_valid_move(user_input):
+            return user_input[0]
+        prompt("Sorry, that's not a valid move! Try again:")
 
 def display_hands(player_hand, dealer_hand, final=False):
-    player_cards = [card for card_info in player_hand # Extract this logic out
+    player_cards = [card for card_info in player_hand # Extract this logic out into nested function
                     for card in card_info.keys()]
     dealer_cards = [card for card_info in dealer_hand
                    for card in card_info.keys()]
-    prompt(f"Your hand:   {', '.join(player_cards[:-1])}" 
+    print(f"Your hand:   {', '.join(player_cards[:-1])}" 
             f"{',' if len(player_cards) > 2 else ''} and {player_cards[-1]} ")
     
     if not final:
-        prompt(f"Dealer hand: {', '.join(dealer_cards[1:])}"
+        print(f"Dealer hand: {', '.join(dealer_cards[1:])}"
                f"{',' if len(dealer_cards) > 2 else ''} and ? of ?")
     else:
-        prompt(f"Dealer hand: {', '.join(dealer_cards[:-1])}"
+        print(f"Dealer hand: {', '.join(dealer_cards[:-1])}"
                f"{',' if len(dealer_cards) > 2 else ''} and {dealer_cards[-1]}")
     print('')
 
 def player_turn(deck, player_hand, dealer_hand):
-    os.system('clear')
+    clear_screen()
     while get_hand_value(player_hand) != MAX_HAND:
         if is_bust(player_hand):
             break
         
         display_hands(player_hand, dealer_hand)
-        sim_depth = int(input('Need a hint? Set your desired simulation depth: '))
-        if sim_depth: # need to fix, made easy for testing
-            run_simulation(deck, player_hand, dealer_hand, sim_depth)
-            input('To return to the game, press [enter]')
+        prompt_hint(deck, player_hand, dealer_hand)
+        # sim_depth = int(input('Need a hint? Set your desired simulation depth: '))
+        # if sim_depth: # need to fix, made easy for testing
+        #     run_simulation(deck, player_hand, dealer_hand, sim_depth)
+        #     input('To return to the game, press [enter]')
 
-        os.system('clear')
+        clear_screen()
         prompt('As a reminder, here are your cards:\n')
         display_hands(player_hand, dealer_hand)
 
-        if get_player_move() == 'h':
+        if prompt_player_move() == 'h':
             os.system('clear')
             prompt("You've chosen to hit! Good luck.\n")
             hit(deck, player_hand)
@@ -136,8 +133,15 @@ def player_turn(deck, player_hand, dealer_hand):
     return player_hand
 
 def dealer_turn(deck, dealer_hand, player_hand):
+    
+    def dealer_hits(deck, hand):
+        while get_hand_value(hand) < DEALER_MAX:
+            hit(deck, hand)
+
+        return hand
+    
     if not is_bust(player_hand):
-        dealer_hand = get_dealer_hits(deck, dealer_hand)
+        dealer_hand = dealer_hits(deck, dealer_hand)
 
     return dealer_hand
 
@@ -201,7 +205,7 @@ def run_simulation(deck, player_hand, dealer_hand, sim_depth):
 
 def display_sim_results(simulation_results, current_hand, sim_depth): # nest in the above
     clear_screen()
-    prompt(f'Simulation results: {sim_depth} iterations')
+    prompt(f'Simulation results with {sim_depth} iterations')
     for key, win_ratio in simulation_results.items():
         if key == current_hand:
             print(f'Staying at {key} has a {win_ratio * 100:.2f}% win rate.')
@@ -209,8 +213,45 @@ def display_sim_results(simulation_results, current_hand, sim_depth): # nest in 
             print(f'Hitting to {key} has a {win_ratio * 100:.2f}% win rate.')
     
     best_choice = max(simulation_results, key=simulation_results.get)
-    print('\nAt this point in the game, the best strategy is to '
+    print('\nAt this point in the game, the optimal strategy is to '
           f"{'stay' if best_choice == current_hand else 'hit'}.\n")
+
+def prompt_user_decision():
+    while True:
+        decision = input().casefold()
+        if is_valid_decision(decision):
+            return decision[0]
+        print('Sorry, please input [y/n]!')
+
+def is_valid_decision(user_input):
+    return user_input in USER_DECISION
+
+def prompt_sim_depth():
+    print('How many iterations would you like to simulate?\n'
+          'Please choose an number between 100 and 10,001.')
+    while True:
+        sim_depth = input()
+        if is_valid_sim_depth(sim_depth):
+            return int(sim_depth)
+        print('\nSorry, you need to input an integer between 100 and 10,000.')
+
+def is_valid_sim_depth(depth):
+    return depth.isdigit() and (99 < int(depth) < 10001)
+
+def prompt_hint(deck, player_hand, dealer_hand):
+    prompt('Need a hint? Do you want to run a simulation? [y/n]\n')
+
+    decision = prompt_user_decision()
+    if decision == 'y':
+
+        clear_screen()
+        sim_depth = prompt_sim_depth()            
+        run_simulation(deck, player_hand, dealer_hand, sim_depth)
+
+        input('To return to the game, press [enter]')
+        clear_screen()
+        prompt('As a reminder, here are your cards:\n')
+        display_hands(player_hand, dealer_hand)
 
 def play_round(deck, player_hand, dealer_hand):
     player_hand = player_turn(deck, player_hand, dealer_hand)
@@ -219,9 +260,11 @@ def play_round(deck, player_hand, dealer_hand):
     
     return outcome, player_hand, dealer_hand
 
-def play_twenty_one():
-    os.system('clear')
-    prompt('Welcome to Twenty One!\n') # Extract out the prompts
+def prompt_play_again():
+    prompt('Would you like to play again?')
+    return prompt_user_decision()
+
+def play_multiple_rounds():
     while True:
         deck = initialize_deck()
         prompt("Cards are dealt! Press [enter] to play.")
@@ -231,10 +274,16 @@ def play_twenty_one():
         outcome, player_hand, dealer_hand = play_round(deck, player_hand, dealer_hand)
         display_winner(outcome, player_hand, dealer_hand)
         
-        prompt('Would you like to play again?')
-        if input() == 'n':
+        if prompt_play_again() == 'n':
             prompt('Thanks for playing!')
             break
-        os.system('clear')
+        
+        clear_screen()
+
+def play_twenty_one():
+    clear_screen()
+    prompt('Welcome to Twenty One!\n') # Extract out the prompts
+    play_multiple_rounds() # Display final outcome?
+
 
 play_twenty_one()
--- spot_lead/PY101_109_04042024.py
+++ spot_lead/PY101_109_04042024.py
@@ -3,29 +3,38 @@ Mutability and immutability
 - variables as pointers
 - shallow and deep copies
 """
+############################################
+
+# Point: variable aliasing, variables as pointers
 
 lst_1 = [num for num in range(5)]
+
 # print(lst_1)
 
 lst_2 = lst_1
 lst_1[2] = 'New value!'
 # print(lst_2)
 
-#######
+############################################
+
+# Point: mutability?, shallow copying, variables as pointers & nestd objects
 
 lst_1 = [[num, num * 2] for num in range(5)]
+
 #print(lst_1)
-tup_1 = tuple(lst_1)
+tup_1 = tuple(lst_1) # note immutable data type, shallow copy with constructor function
 
 tup_1[1][1] = 'New val'
 # print(tup_1)
  
-#######
+############################################
+
+# Point: more on copies
 
 dict_1 = {str(num): num for num in range(5)}
 # print(dict_1)
 
-dict_2 = dict(dict_1)
+dict_2 = dict(dict_1) # copy with constructor function
 dict_2['1'] = 'one'
 
 # print(dict_1)
@@ -33,17 +42,21 @@ dict_2['1'] = 'one'
 dict_1 = {str(num): [num, num * 2] for num in range(5)}
 # print(dict_1)
 
-dict_2 = dict_1
+dict_2 = dict(dict_1) # only a shallow copy, how might we fix?
 dict_2['0'][1] = 'new value'
 
 # print(dict_1)
 
-#######
+############################################
 
 """
 Variable scope
 - variable shadowing
 """
+
+############################################
+
+# Point: variable shadowing
 
 number = 10
 number_2 = 15
@@ -53,9 +66,11 @@ def add_numbers(number, number_2):
 
     return number + number_2
 
-print(add_numbers(number, number_2))
+# print(add_numbers(number, number_2))
 
-#######
+############################################
+
+# Point: More variable shadowing, returning a value vs. mutation
 
 my_list = ['a', 'b', ['c', 'd']]
 
@@ -70,8 +85,82 @@ def mutate_list(lst):
 
     return my_lst
     
-print(mutate_list(my_list))
-print(my_list)
+# print(mutate_list(my_list))
+# print(my_list)
+
+############################################
+
+# Point: Nested functions & variable scope
+
+squares = {num: num ** 2 for num in range(5)}
+
+def modify_dictionary(my_dict):
+
+    def make_modifications(my_dict):
+
+        # my_dict = {'a': 1} # Next try commenting this out
+
+        def make_more_modifications(my_dict):
+            
+            my_dict['a'] = 2
+            print(f'At "make more" ==> {my_dict}')
+    
+        make_more_modifications(my_dict)
+        
+        print(f'At "make mod" ==> {my_dict}')
+    
+    make_modifications(squares)
+
+    print(f'At "mod dict" ==> {my_dict}')
+
+# print(f'Outside ==> {modify_dictionary(squares)}')
+# print(squares)
+# reassign
+
+############################################
+
+# Point: Immutability, what actually is a function doing?
+
+a_string = 'Here is a string!'
+
+def string_shift(my_string):
+    for char in my_string:
+        char += '1'
+    
+    return my_string
+
+# print(string_shift(a_string))
+
+############################################
+
+# Point: Why is this still wrong? What actually is a function doing?
+
+a_string = 'Here is a string!'
+
+def string_shift_2(my_string):
+    my_list = list(my_string)
+    for char in my_list:
+        char += '1'
+
+    return ''.join(my_list)
+
+# print(string_shift_2(a_string))
+
+############################################
+
+# Why is this different from the above?
+
+a_string = 'Here is a string!'
+
+def string_shift_3(my_string):
+    my_list = list(my_string)
+    
+    for idx in range(len(my_string)):
+        my_list[idx] += '1'
+    
+    return ''.join(my_list)
+
+print(string_shift_3(a_string))
 
 
 """
@@ -80,6 +169,81 @@ Functions
 
 """
 
-"""
+# Point: Print vs. return value
 
-"""
+def do_something(some_argument): # What do we call the variable in the parentheses?
+    print(some_argument)
+
+# print(do_something("Here's an argument!"))
+
+############################################
+
+# Talk me through the following function at both an implementation level
+# and user level
+
+from pprint import pprint
+
+SUITS = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+FACE_VALUES = {'Jack': 10, 'Queen': 10, 'King': 10, 'Ace': 11}
+
+def create_deck_of_cards(card_suits, face_card_values):
+    
+    def generate_rank_list():
+        non_face_cards = []
+        face_cards = []
+
+        for number in range(2, 11):
+            non_face_cards.append(number)
+        
+        for face_card in face_card_values:
+            face_cards.append(face_card)
+        
+        return non_face_cards + face_cards
+    
+    def initialize_deck(rank_list):
+        deck = []
+
+        for suit in card_suits:
+            for rank in rank_list:
+                if isinstance(rank, int):
+                    deck.append({f'{rank} of {suit}': rank})
+                else:
+                    deck.append({f'{rank} of {suit}': face_card_values[rank]})
+        
+        return deck
+    
+    rank_list = generate_rank_list()
+    deck = initialize_deck(rank_list)
+
+    return deck
+
+# pprint(create_deck_of_cards(SUITS, FACE_VALUES))
+
+# Method vs. function; concatenate vs. merge?; array?
+
+############################################
+
+# Optimizing with comprehensions, may be too hard
+
+def create_deck_of_cards_2(card_suits, face_card_values):
+    
+    def generate_rank_list():
+        non_face_cards = [number for number in range(2, 11)]
+        face_cards = [face_card for face_card in face_card_values]
+
+        return non_face_cards + face_cards
+    
+    def initialize_deck(rank_list):
+        deck = [{f'{rank} of {suit}': rank} if isinstance(rank, int)
+                 else {f'{rank} of {suit}': face_card_values[rank]}
+                 for suit in card_suits
+                 for rank in rank_list]
+
+        return deck
+    
+    rank_list = generate_rank_list()
+    deck = initialize_deck(rank_list)
+
+    return deck
+
+# pprint(create_deck_of_cards_2(SUITS, FACE_VALUES))
